4bdebec1e1861b8d2e03884e74a69b59d3a1708b
fcc4mv <fcc4mv@gmail.com>
2016-06-29 15:33:07 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+	// require express so that we can build an express app
+var express = require('express');
+	// require path so that we can use path stuff like path.join
+var path = require('path');
+	// instantiate the app
+var app = express();
+
+	// set up a static file server that points to the "client" directory
+app.use(express.static(path.join(__dirname, '/client')));
+
+var bodyParser = require('body-parser'); 
+app.use(bodyParser.json());
+
+
+	// This goes in our server.js file so that we actually use the mongoose config file!
+	// Need to require this BEFORE you require the routes.js file.
+require('./server/config/mongoose.js');
+	// requires and runs the code from our routes.js file and passes it app
+	// so that we can attach our routing rules to our express application!
+require('./server/config/routes.js')(app);
+
+app.listen(8000, function() {
+	console.log('cool stuff on: 8000');
+});
---
+++ server/config/routes.js
@@ -0,0 +1,19 @@
+	// First, at the top of your routes.js file you'll have to require the controller
+var customers = require('./../controllers/customers.js');
+	// This is our routes.js file located in server/config/routes.js
+	// This is where we will define all of our routing rules!
+	// We will have to require this in the server.js file (and pass it app!)
+module.exports = function(app) {
+	// verb: get, plural of target as the URI is the RESTful index method (it returns all friends)
+	app.get('/customers', function(req, res) {
+		customers.index(req, res);
+	});
+
+	app.post('/addFriend', function(req, res){
+		customers.create(req, res);
+	});
+
+	app.post('/deleteFriend/:id', function(req, res){
+		customers.delete(req, res);
+	})
+};
